f6b30f120650036d08878d6954a535984565fbf1
Jonathan Avila <jonathanavila.dev@gmail.com>
2023-01-18 18:59:21 -0500

    Added spaceship, and refactored bg_color

--- ship.py
+++ ship.py
@@ -1,5 +1,15 @@
 import pygame
-def __init__(self, ai_game):
-    self.screen = ai_game.screen
-    self.
-    
+
+class Ship:
+    def __init__(self, ai_game):
+        self.screen = ai_game.screen
+        self.screen_rect = ai_game.screen.get_rect()
+
+        self.image = pygame.image.load("ship.bmp")
+        self.rect = self.image.get_rect()
+
+        self.rect.midbottom = self.screen_rect.midbottom
+
+    def blitme(self):
+        self.screen.blit(self.image, self.rect)
+        
