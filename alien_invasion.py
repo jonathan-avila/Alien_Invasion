2cf45c9619e43fcf63187a23e686bf6b86661656
Jonathan Avila <jonathanavila.dev@gmail.com>
2023-01-22 22:55:20 -0500

    Added bidirectional mobility to spaceship

--- alien_invasion.py
+++ alien_invasion.py
@@ -13,15 +13,31 @@ class AlienInvasion:
 
     def run_game(self):
         while True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self._check_events()
+            self.ship.update()
+            self._update_screen()
+    def _check_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = True
 
-            self.screen.fill(self.settings.bg_color)
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
 
-            self.ship.blitme()
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
 
-            pygame.display.flip()
+    def _update_screen(self):
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+
+        pygame.display.flip()
 
 if __name__ == "__main__":
     ai = AlienInvasion()
