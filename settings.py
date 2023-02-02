f20517fd29bf1f61f0890aeece12d6f617dd0749
Jonathan Avila <jonathanavila.dev@gmail.com>
2023-02-01 19:53:48 -0500

    added bullets class

--- settings.py
+++ settings.py
@@ -4,3 +4,10 @@ class Settings:
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (100,100,100)
+
+        #Bullet settings
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60,60,60)
+        
